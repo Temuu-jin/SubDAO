f78b752781ccde9353c868d926b059e12b1a14e6
Temuu-jin <temucin.d.k@gmail.com>
2023-11-08 15:11:28 +0100

    Fix apolloClient HTTPLink

--- util/apolloClient.ts
+++ util/apolloClient.ts
@@ -2,10 +2,19 @@ import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc';
 
 export const { getClient } = registerApolloClient(() => {
+  // This is for the GitHub GraphQL endpoint
+  const flyioLink = new HttpLink({
+    uri: 'https://subdao.fly.dev/api/graphql',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
   // This is for the local GraphQL endpoint
-  const link = new HttpLink({
+  const localLink = new HttpLink({
     // uri: '/api/graphql',
-    uri: '/api/graphql',
+    uri: 'http://localhost:3000/api/graphql',
     credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json',
@@ -13,6 +22,14 @@ export const { getClient } = registerApolloClient(() => {
   });
 
   // This is needed because two endpoints are being used
+  const link = split(
+    // Split based on the target URI
+    ({ operationName }) => {
+      return operationName.startsWith('https://subdao.fly.dev');
+    },
+    flyioLink,
+    localLink,
+  );
 
   return new ApolloClient({
     cache: new InMemoryCache(),
