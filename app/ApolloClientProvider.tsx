e70861b53d8608cd4d9bb946e66c07744289c463
Temuu-jin <temucin.d.k@gmail.com>
2023-11-17 15:45:08 +0100

    Apollo Deploy Settings changed

--- app/ApolloClientProvider.tsx
+++ app/ApolloClientProvider.tsx
@@ -10,7 +10,9 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: 'https://subdao.fly.dev/api/graphql',
+
+    // uri: 'http://localhost:3000/api/graphql',
     fetchOptions: { cache: 'no-store' },
   });
 
